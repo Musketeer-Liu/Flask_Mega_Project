01abd0f7d2bc0e6866216779d370518e53fb16d1
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-17 14:45:44 -0800

    Avatar

--- app/models.py
+++ app/models.py
@@ -25,6 +25,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
