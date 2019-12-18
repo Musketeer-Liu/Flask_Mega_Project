0b72b543bf267d33ca6c06e4d89e9474de57171c
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-17 16:04:19 -0800

    Error Handling and Debugging

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
 from config import Config
-from app import routes, models, errors
 
 
 app = Flask(__name__)
@@ -38,7 +37,7 @@ if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/flaskblog.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Fomatter(
+    file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s: %(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
