784814b4917963b1e9bc9d46745fa6ae1f5af180
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-18 15:13:22 -0800

    Explore Page

--- app/routes.py
+++ app/routes.py
@@ -30,6 +30,13 @@ def index():
     return render_template('index.html', title="Home Page", form=form, posts=posts)
 
 
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
