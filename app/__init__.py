1f2594bae1f0dfc12c052de65b7066fd4c8533f0
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-19 00:28:38 -0800

    Debug for Translate

--- app/__init__.py
+++ app/__init__.py
@@ -56,7 +56,7 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGESS'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
     # return 'es'
 
 
