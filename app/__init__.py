1aa5733047a88e00858ea4a9af2ff7f2de6435f6
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-18 22:20:04 -0800

    Beautify the web with Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
