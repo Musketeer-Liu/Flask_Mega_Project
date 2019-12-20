eb82c69820907d8740f57e1fee04044793ed8ddd
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-20 12:52:17 -0800

    WIP: Message with not attribute messages_received

--- app/models.py
+++ app/models.py
@@ -61,7 +61,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship(
+        'Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
@@ -69,6 +70,14 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    message_sent = db.relationship(
+        'Message', foreign_keys='Message.sender_id',
+        backref='author', lazy='dynamic')
+    message_received = db.relationship(
+        'Message', foreign_keys='Message.recipient_id',
+        backref='recipient', lazy='dynamic')
+    last_messages_read_time = db.Column(db.DateTime)
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -116,6 +125,10 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    def new_message(self):
+        last_read_time = self.last_messages_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
+
 
 @login.user_loader
 def load_user(id):
@@ -136,3 +149,14 @@ class Post(SearchableMixin, db.Model):
 
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
